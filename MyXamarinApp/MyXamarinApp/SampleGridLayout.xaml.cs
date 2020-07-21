610b107cfbca85367cdf22d8364a0084f1df93d1
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-21 13:51:22 +0700

    event handler click

--- MyXamarinApp/MyXamarinApp/SampleGridLayout.xaml.cs
+++ MyXamarinApp/MyXamarinApp/SampleGridLayout.xaml.cs
@@ -15,6 +15,34 @@ namespace MyXamarinApp
         public SampleGridLayout()
         {
             InitializeComponent();
+            btnTambah.Clicked += BtnProses_Clicked;
         }
+
+        private void BtnProses_Clicked(object sender, EventArgs e)
+        {
+            var bil1 = Convert.ToDouble(entryBil1.Text);
+            var bil2 = Convert.ToDouble(entryBil2.Text);
+            double hasil = 0;
+            var btn = (Button)sender;
+
+            switch (btn.Text)
+            {
+                case "Tambah":
+                    hasil = bil1 + bil2;
+                    break;
+                case "Kurang":
+                    hasil = bil1 - bil2;
+                    break;
+                case "Kali":
+                    hasil = bil1 * bil2;
+                    break;
+                case "Bagi":
+                    hasil = bil1 / bil2;
+                    break;
+            }
+            entryHasil.Text = hasil.ToString();
+        }
+
+      
     }
 }
