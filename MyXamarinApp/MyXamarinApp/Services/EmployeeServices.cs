c6b3e7156e5bfd97b078c7eac0029e30624f4a48
Erick Kurniawan <erick.kurniawan@gmail.com>
2020-07-24 09:48:54 +0700

    menambahkan services get all

--- MyXamarinApp/MyXamarinApp/Services/EmployeeServices.cs
+++ MyXamarinApp/MyXamarinApp/Services/EmployeeServices.cs
@@ -1,11 +1,39 @@
-﻿using System;
+﻿using MyXamarinApp.Models;
+using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
+using System.Net.Http;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace MyXamarinApp.Services
 {
     public class EmployeeServices
     {
+        private HttpClient _client;
+        public EmployeeServices()
+        {
+            _client = new HttpClient();
+        }
 
+        public async Task<List<Employee>> GetAll()
+        {
+            List<Employee> lstEmp = new List<Employee>();
+            var uri = new Uri($"{Helpers.Helpers.restUrl}/api/Employee");
+            try
+            {
+                var response = await _client.GetAsync(uri);
+                if (response.IsSuccessStatusCode)
+                {
+                    var content = await response.Content.ReadAsStringAsync();
+                    lstEmp = JsonConvert.DeserializeObject<List<Employee>>(content);
+                }
+                return lstEmp;
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
     }
 }
